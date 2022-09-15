76571aaaebb9213897ecb272db04742080d15a86
Phelipi <ggizmeuamigo@gmail.com>
2022-09-15 09:30:58 -0300

    first commit

---
+++ netfilx2/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HeaderComp from '@/components/HeaderComp.vue';
+import MainComp from './components/MainComp.vue';
+</script>
+
+<template>
+    <HeaderComp/>
+  <main>
+    <MainComp/>
+  </main>
+  <RouterView />
+</template>
+
+<style ></style>
